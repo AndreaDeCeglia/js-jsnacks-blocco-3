1c72c7da7a4dacb96a76e9840ac44effa1499cf1
AndreaDeCeglia <deceglia89@gmail.com>
2022-10-26 15:17:54 +0200

    second snack done

--- main.js
+++ main.js
@@ -6,17 +6,45 @@
 //e a inserirli nell’array fino a quando 
 //la somma degli elementi è minore di 50.
 
-let array = [];
-console.log(array);
+// let array = [];
+// console.log(array);
+
+// let sum = 0;
+
+// while( sum < 50 ){
+//     let number = parseInt( prompt('write a number'));
+//     console.log(number);
+//     sum += number;
+//     console.log(sum);
+//     array.push(number);
+// }
+
+// console.log(array);
+
+//*************************************** */
+
+
+//*Snack2*
+//Il software deve chiedere per 5 volte 
+//all’utente di inserire un numero.
+//Il programma stampa la somma di 
+//tutti i numeri inseriti.
+
+let i = 0;
 
 let sum = 0;
 
-while( sum < 50 ){
+console.log(sum);
+
+while( i < 5 ){
+    
     let number = parseInt( prompt('write a number'));
     console.log(number);
+
     sum += number;
     console.log(sum);
-    array.push(number);
+
+    i++
 }
 
-console.log(array);
+console.log(`the sum is `, sum);
